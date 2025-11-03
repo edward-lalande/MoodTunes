cf1c36d5aecbce4f6f3ba5f57a40f9eb22915d74
Chaegnal <eva.legrand@epitech.eu>
2025-11-03 21:29:43 +0900

    feat(loginPage): adds entire login page, not yet linked with backend

--- mobile/app/src/main/java/com/example/moodtunes/MainActivity.kt
+++ mobile/app/src/main/java/com/example/moodtunes/MainActivity.kt
@@ -2,18 +2,37 @@ package com.example.moodtunes
 
 import MoodCard
 import Music
+import android.graphics.drawable.Icon
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,12 +40,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moodtunes.components.MoodTunesButtonField
 import com.example.moodtunes.components.MoodTunesTextField
 import com.example.moodtunes.ui.theme.MoodTunesTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,39 +78,189 @@ class MainActivity : ComponentActivity() {
 fun Greeting(modifier: Modifier = Modifier) {
     var searchText by remember { mutableStateOf("") }
     Column(
-        modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        MoodCard(
-          mood = Mood.Energetic,
-          time = "2:30 PM",
-          music = Music(
-            name = "Good 4 U",
-            artist = "Olivia Rodrigo",
-            releaseDate = "2021",
-          ),
-          modifier = modifier
-              .fillMaxWidth()
-              .padding(16.dp)
-        )
-        MoodTunesTextField(
-            text = searchText,
-            onTextChange = { newText -> searchText = newText },
-            outlineColor = Color.Gray,
-            fillColor = Color(red = 6.7f / 255f, green = 12f / 255f, blue = 25f / 255f),
-            placeholder = "Search Your moods",
-            leadingIcon = Icons.Default.Search,
-            modifier = Modifier.padding(horizontal = 16.dp)
-        )
-        MoodTunesButtonField(
-            text = "Search",
-            onClick = {},
-            backgroundColor = Color.Red,
-            contentColor = Color.White,
-        )
+        LoginPage()
     }
 }
+
+@Composable
+fun LoginPage() {
+    LoginBackground {
+        var emailText by remember { mutableStateOf("") }
+        var passwordText by remember { mutableStateOf("") }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Card(
+                shape = CircleShape,
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFF5B21B6),
+                    contentColor = Color.White,
+                ),
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.musicnote),
+                    contentDescription = "Music Icon",
+                    colorFilter = ColorFilter.tint(Color.White),
+                    modifier = Modifier
+                        .padding(30.dp)
+                        .size(30.dp)
+                )
+            }
+
+            Text(
+                text = "MoodTunes",
+                color = Color.White,
+                fontSize = 38.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Text(
+                text = "Music that matches your mood",
+                color = Color.Gray,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.SemiBold,
+                modifier = Modifier.padding(8.dp)
+            )
+
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF1DB954),
+                    contentColor = Color.White
+                ),
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier
+                    .padding(32.dp)
+                    .fillMaxWidth()
+                    .height(56.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.spotify),
+                        contentDescription = "Spotify Icon",
+                        modifier = Modifier
+                            .size(45.dp)
+                            .padding(end = 12.dp)
+                    )
+
+                    Text(
+                        text = "Continue with Spotify",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        color = Color.White
+                    )
+                }
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+            ) {
+                HorizontalDivider(
+                    Modifier.weight(1f),
+                    DividerDefaults.Thickness,
+                    color = Color.Gray
+                )
+                Text(
+                    text = "or",
+                    Modifier.padding(horizontal = 8.dp),
+                    color = Color.Gray
+                )
+                HorizontalDivider(
+                    Modifier.weight(1f),
+                    DividerDefaults.Thickness,
+                    color = Color.Gray
+                )
+            }
+            MoodTunesTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                text = emailText,
+                onTextChange = { newText -> emailText = newText },
+                outlineColor = Color(0xFF5B21B6),
+                placeholder = "Email",
+                backgroundColor = Color(0xFF1A1A1A).copy(alpha = 0.1f),
+                fillColor = Color.Transparent,
+                textColor = Color.White
+            )
+
+            MoodTunesTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp, horizontal = 16.dp),
+                text = passwordText,
+                onTextChange = { newText -> passwordText = newText },
+                outlineColor = Color(0xFF5B21B6),
+                placeholder = "Password",
+                backgroundColor = Color(0xFF1A1A1A).copy(alpha = 0.1f),
+                fillColor = Color.Transparent,
+                textColor = Color.White
+            )
+
+            MoodTunesButtonField(
+                onClick = {},
+                text = "Sign In",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                backgroundColor = Color(0xFF5B21B6),
+                textStyle = TextStyle(
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                ),
+            )
+
+            Row () {
+                Text(
+                    text = "Don't have an account?",
+                    color = Color.Gray,
+                    fontSize = 16.sp
+                )
+
+                Text(
+                    text = "Sign up",
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    modifier = Modifier
+                        .clickable { }
+                        .padding(horizontal = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun LoginBackground(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF312B90),
+                        Color.Black,
+                        Color(0xFF312B90)
+                    ),
+                )
+            )
+    ) {
+        content()
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun PreviewMoodCard() {
