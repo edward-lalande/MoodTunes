1de2a845655bb59b9cbfb51fd3da6be9cbf984d1
Chaegnal <eva.legrand@epitech.eu>
2025-11-04 11:49:57 +0900

    fix(MoodTunes): fixes warning

--- mobile/app/src/main/java/com/example/moodtunes/MainActivity.kt
+++ mobile/app/src/main/java/com/example/moodtunes/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.example.moodtunes.navigation.MoodNavGraph
+import com.example.moodtunes.pages.MoodNavGraph
 import com.example.moodtunes.ui.theme.MoodTunesTheme
 
 class MainActivity : ComponentActivity() {
