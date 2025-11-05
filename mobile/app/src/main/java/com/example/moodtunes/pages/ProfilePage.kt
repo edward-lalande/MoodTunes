28a7db031f4ea1e138599f16fb55e10eca572150
edward-lalande <edward.lalande@epitech.eu>
2025-11-05 12:52:43 +0900

    fix(ProfilePage): adding Background component

--- mobile/app/src/main/java/com/example/moodtunes/pages/ProfilePage.kt
+++ mobile/app/src/main/java/com/example/moodtunes/pages/ProfilePage.kt
@@ -1,36 +1,27 @@
 package com.example.moodtunes.pages
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
+import com.example.moodtunes.components.Background
 import com.example.moodtunes.components.BottomBar
 import com.example.moodtunes.components.PageSelected
 
 @Composable
 fun ProfilePage(navController: NavController) {
-    Scaffold (
-        modifier = Modifier
-            .background(
-                brush = Brush.linearGradient(
-                    colors = listOf(
-                        Color(0xFF312B90),
-                        Color.Black,
-                        Color(0xFF312B90)
-                    ),
+    Background {
+        Scaffold (
+            content = { innerPadding ->
+                Text("ProfilePage",
+                    modifier = Modifier.padding(innerPadding),
+                    color = Color.White
                 )
-            ),
-        content = { innerPadding ->
-            Text("ProfilePage",
-                modifier = Modifier.padding(innerPadding),
-                color = Color.White
-            )
-        },
-        bottomBar = { BottomBar(navController, PageSelected.Profile) }
-    )
+            },
+            bottomBar = { BottomBar(navController, PageSelected.Profile) }
+        )
+    }
 }
