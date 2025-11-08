e469b9d90bd221ccb5f0ef821b7e0bfcabd8f5d2
Chaegnal <eva.legrand@epitech.eu>
2025-11-08 17:22:57 +0900

    feat(historyPage): adds history page

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation("androidx.navigation:navigation-compose:2.7.2")
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
